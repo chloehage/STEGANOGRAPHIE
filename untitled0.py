d3a98f7a084d3afe6a80836fe1f73ea6ceb415d9
cococode <113681517+chloehage@users.noreply.github.com>
2022-11-24 23:33:33 +0100

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -6,30 +6,23 @@ This is a temporary script file.
 from PIL.Image import *
 from PIL import Image
 
-img_fake = open("gouv.jpg")
-img_cacher = open("illuminati.jpg")
+img_cacher = open("gouv.jpg")
+img_fake = open("illuminati.jpg")
 
-list_pixel = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
-lpixels_separation_img = [['0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0']]
-lpixels_separation_img2 = [['0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0']]
+list_pixel = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+]
 pixels_image = img_fake.size
 pixels_image1 = img_cacher.size
 
 
-## TODO: remove this unused code
-"""
-def placer_pixels():
-    rconv = int(list_pixel[0])
-    vconv = int(list_pixel[1])
-    bconv = int(list_pixel[2])
-    img.putpixel(x,y),(rconv, vconv, bconv)
-"""
-
 def cacher(image_front, image_back):
     """
-    Cette fonction récupère les codes r,v,b de chaque pixel 
-    des deux images (image_front et image_back) et  les convertie 
-    en binaire pour les redistribuer et creer les pixels d'une troisième image 
+    Cette fonction récupère les codes r,v,b de chaque pixel
+    des deux images (image_front et image_back) et  les convertie
+    en binaire pour les redistribuer et creer les pixels d'une troisième image
     (img_coder).
     """
     pixels_image = image_front.size
@@ -37,7 +30,7 @@ def cacher(image_front, image_back):
     for x in range(pixels_image[0]):
         for y in range(pixels_image[1]):
             # on récupère les pixels à la position x,y de image_front
-            (r_fake, v_fake, b_fake) = image_front.getpixel((x,y))
+            (r_fake, v_fake, b_fake) = image_front.getpixel((x, y))
             # conversion du code RVB en binaire
             r_wait = bin(r_fake)
             v_wait = bin(v_fake)
@@ -46,17 +39,17 @@ def cacher(image_front, image_back):
             v_wait = str(v_wait[2:])
             b_wait = str(b_wait[2:])
             if len(r_wait) < 8:
-                r_wait = (8-len(r_wait))*'0' + r_wait
+                r_wait = (8 - len(r_wait)) * "0" + r_wait
             if len(v_wait) < 8:
-                v_wait = (8-len(v_wait))*'0' + v_wait
+                v_wait = (8 - len(v_wait)) * "0" + v_wait
             if len(b_wait) < 8:
-                b_wait = (8-len(b_wait))*'0' + b_wait
+                b_wait = (8 - len(b_wait)) * "0" + b_wait
             # découpage du code RVB pour récupérer les 5 bits de point fort
             r_fort = r_wait[:5]
             v_fort = v_wait[:5]
             b_fort = b_wait[:5]
             # on récupère les pixels à la position x,y de image_back
-            (r2_cacher, v2_cacher, b2_cacher) = image_back.getpixel((x,y))
+            (r2_cacher, v2_cacher, b2_cacher) = image_back.getpixel((x, y))
             # conversion du code RVB en binaire
             r2_wait = bin(r2_cacher)
             v2_wait = bin(v2_cacher)
@@ -65,16 +58,16 @@ def cacher(image_front, image_back):
             v2_wait = str(v2_wait[2:])
             b2_wait = str(b2_wait[2:])
             if len(r2_wait) < 8:
-                r2_wait = (8-len(r2_wait))*'0' + r2_wait
+                r2_wait = (8 - len(r2_wait)) * "0" + r2_wait
             if len(v2_wait) < 8:
-                v2_wait = (8-len(v2_wait))*'0' + v2_wait
+                v2_wait = (8 - len(v2_wait)) * "0" + v2_wait
             if len(b2_wait) < 8:
-                b2_wait = (8-len(b2_wait))*'0' + b2_wait
+                b2_wait = (8 - len(b2_wait)) * "0" + b2_wait
             # découpage du code RVB pour récupérer les 3 bits de point faible
             r2_fort = r2_wait[:3]
             v2_fort = v2_wait[:3]
             b2_fort = b2_wait[:3]
-            # on assemble les 5 bits de image_front et les 3 de image_back 
+            # on assemble les 5 bits de image_front et les 3 de image_back
             r = r_fort + r2_fort
             v = v_fort + v2_fort
             b = b_fort + b2_fort
@@ -83,21 +76,22 @@ def cacher(image_front, image_back):
             list_pixel[1] = v
             list_pixel[2] = b
             # reconversion en décimal
-            rconv = int(list_pixel[0],2)
-            vconv = int(list_pixel[1],2)
-            bconv = int(list_pixel[2],2)
+            rconv = int(list_pixel[0], 2)
+            vconv = int(list_pixel[1], 2)
+            bconv = int(list_pixel[2], 2)
             # formation du pixel sur l'image finale
-            img_coder.putpixel((x,y),(rconv, vconv, bconv))
-    img_coder.save('img_code.jpg')
+            img_coder.putpixel((x, y), (rconv, vconv, bconv))
+    img_coder.save("img_code.jpg")
     img_coder.show()
 
-    
-cacher(img_fake, img_cacher)
-img_code = open('img_code.jpg')
 
-def decoder(img_coder) :
+cacher(img_fake, img_cacher)
+img_code = open("img_code.jpg")
+
+
+def decoder(img_coder):
     """
-    Cette fonction part de l'image créée par cacher() 
+    Cette fonction part de l'image créée par cacher()
     et récupère les bits de points forts des deux images cachées.
     """
     pixels_image = img_coder.size
@@ -106,45 +100,44 @@ def decoder(img_coder) :
     for x in range(pixels_image[0]):
         for y in range(pixels_image[1]):
             # on récupère le pixel à la position x,y de img_coder
-            pixels_wait_coder = img_coder.getpixel((x,y))
+            pixels_wait_coder = img_coder.getpixel((x, y))
             rc_wait = bin(pixels_wait_coder[0])
             vc_wait = bin(pixels_wait_coder[1])
             bc_wait = bin(pixels_wait_coder[2])
-            # on récupère les bits de la première image
-            r3_fort = rc_wait[2:7]
-            v3_fort = vc_wait[2:7]
-            b3_fort = bc_wait[2:7]
+            # on supprime le 0b
+            r0b = rc_wait[2:]
+            v0b = vc_wait[2:]
+            b0b = bc_wait[2:]
+            # on ajoute des '0' pour coder sur 8 bits
+            r1 = (8 - len(r0b)) * "0" + str(r0b)
+            v1 = (8 - len(v0b)) * "0" + str(v0b)
+            b1 = (8 - len(b0b)) * "0" + str(b0b)
+            # on récupere les bits de la premiere image
+            r3_fort = r1[:5]
+            v3_fort = v1[:5]
+            b3_fort = b1[:5]
             # on récupère les bits de la deuxième image
-            r3_faible = rc_wait[7:]
-            v3_faible = vc_wait[7:]
-            b3_faible = bc_wait[7:]
-            # formation du code RVB de chaque image
-            lpixels_separation_img[0][:5] = r3_fort
-            lpixels_separation_img[1][:5] = v3_fort
-            lpixels_separation_img[2][:5] = b3_fort
-            lpixels_separation_img2[0][:3] = r3_faible
-            lpixels_separation_img2[1][:3] = v3_faible
-            lpixels_separation_img2[2][:3] = b3_faible
-            # conversion de la liste en chaine de caractère 
-            rchaine = ''.join(lpixels_separation_img[0])
-            vchaine = ''.join(lpixels_separation_img[1])
-            bchaine = ''.join(lpixels_separation_img[2])
+            r3_faible = r1[5:]
+            v3_faible = v1[5:]
+            b3_faible = b1[5:]
+            # on ajoute des '0' pour coder le pixel sur 8 bits
+            r9 = str(r3_fort) + (8 - len(r3_fort)) * "0"
+            v9 = str(v3_fort) + (8 - len(v3_fort)) * "0"
+            b9 = str(b3_fort) + (8 - len(b3_fort)) * "0"
+            r8 = str(r3_faible) + (8 - len(r3_faible)) * "0"
+            v8 = str(v3_faible) + (8 - len(v3_faible)) * "0"
+            b8 = str(b3_faible) + (8 - len(b3_faible)) * "0"
             # conversion en décimal
-            rconv = int(rchaine,2)
-            vconv = int(vchaine,2)
-            bconv = int(bchaine,2)
-            img_decoder.putpixel((x,y),(rconv, vconv, bconv))
-            rchaine1 = ''.join(lpixels_separation_img2[0])
-            vchaine1 = ''.join(lpixels_separation_img2[1])
-            bchaine1 = ''.join(lpixels_separation_img2[2])
-            rconv1 = int(rchaine1,2)
-            vconv1 = int(vchaine1,2)
-            bconv1 = int(bchaine1,2)
-            img_decoder1.putpixel((x,y),(rconv1, vconv1, bconv1))
+            rconv = int(r9, 2)
+            vconv = int(v9, 2)
+            bconv = int(b9, 2)
+            img_decoder.putpixel((x, y), (rconv, vconv, bconv))
+            rconv1 = int(r8, 2)
+            vconv1 = int(v8, 2)
+            bconv1 = int(b8, 2)
+            img_decoder1.putpixel((x, y), (rconv1, vconv1, bconv1))
     img_decoder.show()
     img_decoder1.show()
 
 
-
 decoder(img_code)
-
